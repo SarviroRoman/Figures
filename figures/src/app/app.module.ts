a144f7687614637d725009e0118771ad85de0f9b
Roman Sarviro <roman.sarviro@mail.ru>
2019-02-24 17:02:04 +0300

    Create component "header"

--- figures/src/app/app.module.ts
+++ figures/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { AlertModule } from 'ngx-bootstrap';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
