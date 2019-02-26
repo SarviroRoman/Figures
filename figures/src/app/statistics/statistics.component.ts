331573e3c8708f1dbf47b2caf625e9765aa43080
Roman Sarviro <roman.sarviro@mail.ru>
2019-02-26 16:03:18 +0300

    Was added component 'statistics', containing statistics added figures (from index.html to figures.component.html)

---
+++ figures/src/app/statistics/statistics.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+})
+
+export class StatisticsComponent{
+  constructor() { 
+    
+  }
+
+  ngOnInit() {
+  }
+}
