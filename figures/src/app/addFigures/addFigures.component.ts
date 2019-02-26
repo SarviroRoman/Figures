7cb731f9bf37e7667123019dc57e5cb673988b66
Roman Sarviro <roman.sarviro@mail.ru>
2019-02-26 16:05:23 +0300

    Was added component 'addFigures', containing form to add figures (from index.html to figures.component.html)

---
+++ figures/src/app/addFigures/addFigures.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addFigures',
+  templateUrl: './addFigures.component.html',
+})
+
+export class addFiguresComponent {
+
+  constructor (){
+  }
+
+  ngOnInit() {
+  }
+  
+}
+
