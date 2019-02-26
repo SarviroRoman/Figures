accad4a51fb68fefd05b0c82209bbf53e57bd4d6
Roman Sarviro <roman.sarviro@mail.ru>
2019-02-26 16:01:23 +0300

    Was added component 'figures', containing list added figures (from index.html to figures.component.html)

---
+++ figures/src/app/figures/figures.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-figures',
+  templateUrl: './figures.component.html',
+})
+export class FiguresComponent {
+  
+  constructor() { 
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
