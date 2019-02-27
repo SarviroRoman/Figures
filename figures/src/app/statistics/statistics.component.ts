1c1f6a3bc2271fcd355262217499a2f78f483a64
Roman Sarviro <roman.sarviro@mail.ru>
2019-02-27 16:27:28 +0300

    Update component 'statistics' Now data take from figures objects array

--- figures/src/app/statistics/statistics.component.ts
+++ figures/src/app/statistics/statistics.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 
+interface Figure {
+  type: string;
+  area: number;
+}
+
+const FIGURES: Figure[] = [
+  {
+    type: 'Circle',
+    area: 10,
+  },
+  {
+    type: 'Square',
+    area: 20,
+  },
+  {
+    type: 'Rectangle',
+    area: 15,
+  },
+  {
+    type: 'Triangle',
+    area: 1,
+  },
+];
+
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
 })
 
-export class StatisticsComponent{
-  constructor() { 
-    
-  }
+export class StatisticsComponent {
 
-  ngOnInit() {
-  }
+  figures = FIGURES;
+
 }
