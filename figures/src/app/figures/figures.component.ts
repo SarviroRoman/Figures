dfad1a93835c38ce920271cc84042df1f5a837db
Roman Sarviro <roman.sarviro@mail.ru>
2019-02-27 16:27:10 +0300

    Update component 'figures' Now data take from figures objects array

--- figures/src/app/figures/figures.component.ts
+++ figures/src/app/figures/figures.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 
+interface Figure {
+  type: string;
+  area: number;
+}
+
+const FIGURES: Figure[] = [
+  {
+    type: 'Circle',
+    area: 10,
+  },
+  {
+    type: 'Square',
+    area: 20,
+  },
+  {
+    type: 'Rectangle',
+    area: 15,
+  },
+  {
+    type: 'Triangle',
+    area: 1,
+  },
+];
+
 @Component({
   selector: 'app-figures',
   templateUrl: './figures.component.html',
 })
 export class FiguresComponent {
   
-  constructor() { 
-    
-  }
-
-  ngOnInit() {
-  }
-
+  figures = FIGURES;
+  
 }
