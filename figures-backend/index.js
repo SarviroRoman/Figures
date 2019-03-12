255fbea1df6ebd9d2728c387e7cfe3ac0b9d7362
Roman Sarviro <roman.sarviro@mail.ru>
2019-03-12 10:24:22 +0300

    Was added add figures method on the back-end

--- figures-backend/index.js
+++ figures-backend/index.js
@@ -6,27 +6,34 @@ app.use(cors({
   origin: ['http://localhost:4200'],
   credentials: true,
 }))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 let figures = [
   {
     id: 1,
     type: 'Circle',
-    area: 2000,
+    area: 10,
   },
   {
     id: 2,
     type: 'Square',
-    area: 20,
+    area: 25,
   },
   {
     id: 3,
     type: 'Rectangle',
-    area: 15,
+    area: 21,
   },
   {
     id: 4,
     type: 'Triangle',
-    area: 1,
+    area: 19,
+  }, 
+  {
+    id: 5,
+    type: 'Square',
+    area: 25,
   },
 ];
 
@@ -42,6 +49,20 @@ app.delete('/figures', (request, response) => {
   response.send({success: true});
 })
 
-app.listen(3000, () => {
-  console.log('application works on the port 3000');
+app.post('/figures', (request, response) => {
+  const {type, area} = request.body;
+  let newId = 1;
+
+  if (figures[figures.length-1] !== undefined){
+    newId = figures[figures.length-1].id + 1;
+  } 
+
+  figures.push({
+    id: newId,
+    type,
+    area
+  });
+
+  response.send({success: true, 'id': newId});
 });
+
