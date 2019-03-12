0857a7ec8468df2ed7341ff116a3ccced7ff5549
Roman Sarviro <roman.sarviro@mail.ru>
2019-03-12 10:47:43 +0300

    The 'Triangle' component to add triangles has been rewritten for the back-end.

--- figures/src/app/triangle/triangle.component.ts
+++ figures/src/app/triangle/triangle.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, ValidatorFn, ValidationErrors, FormControl, Validators } from '@angular/forms';
 
-import { Figure } from '../models/figure';
 import { FigureService } from '../services/figure.service';
+import { APP } from '../application-constants';
 
 @Component({
   selector: 'app-triangle',
@@ -11,49 +11,54 @@ import { FigureService } from '../services/figure.service';
 })
 
 export class TriangleComponent{
-  public figures: Figure[];
   private triangleControl : FormGroup;
+  public responseIsSuccess: boolean = false; 
+  public showAlertMessage: boolean = false;
+  public alertMessage: string;
 
   constructor (
     private figureService: FigureService
   ) {}
 
   ngOnInit() {
-    this.getFigures();
-
     this.triangleControl = new FormGroup({
       a : new FormControl(3),
       b : new FormControl(4),
       c : new FormControl(5),
     }, { validators: this.triangleValidator });
-
   }
 
-  private getFigures(): void {
-    this.figureService.getFigures().subscribe(figures => this.figures = figures);
-  }
+  private addTriangle(): void {
+    this.responseIsSuccess = true;
+    setTimeout( () => {
+      const area = this.getTriangleArea();
 
-  private add(type: string, area: number): void {
-    type = type.trim();
-
-    if (!type) { return; }
-    if (!area) { return; }
+      this.figureService.addFigure(
+      {
+        type: APP.types.triangle, 
+        area
+      })
+        .subscribe( response => {
+          if(response.success){
+            this.responseIsSuccess = false;
+            this.showAlertMessage = true;
+            this.alertMessage = `Triangle #${response['id']} with ${Math.round(area * 1000) / 1000} area successfully added`;
+          }
+        } );
+    }, 500)
     
-    this.figureService.addFigure({ type, area } as Figure)
-      .subscribe(figure => this.figures.push(figure));
   }
 
-  public addTriangle(points : object): void {
+  public getTriangleArea(): number {
+    const points = this.triangleControl.value;
 
     const a = points['a'];
     const b = points['b'];
     const c = points['c'];
 
-    const type = 'Triangle';  
     const p = (a+b+c)/2;
-    const area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    return Math.sqrt(p*(p-a)*(p-b)*(p-c));
 
-    this.add(type, area);
   }
 
   triangleValidator: ValidatorFn = (triangleControl: FormGroup): ValidationErrors | null => {
